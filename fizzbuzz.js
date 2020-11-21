5410bc3dd0620bc0ddbf8ce040d62ff8c0ffdade
Marco Emrich <marco.emrich@codecentric.de>
2020-11-21 15:59:48 +0100

    it5

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,9 +1,9 @@
-const divisibleBy3 = n => n % 3 === 0
-const divisibleBy5 = n => n % 5 === 0
+const divisibleBy = (div, n) => n % div === 0;
+const divisibleBy3 = (n) => divisibleBy(3, n);
+const divisibleBy5 = (n) => divisibleBy(5, n);
 
 export default (number) => {
-  if (divisibleBy3(number))  return "fizz";
-  if (divisibleBy5(number))  return "buzz";
-  return String(number)
+  if (divisibleBy3(number)) return "fizz";
+  if (divisibleBy5(number)) return "buzz";
+  return String(number);
 };
-
