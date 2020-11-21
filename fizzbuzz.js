7ede200227a51e5e3e61e29ae6c77275841c137c
Marco Emrich <marco.emrich@codecentric.de>
2020-11-21 15:56:44 +0100

    it4

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,4 +1,9 @@
 const divisibleBy3 = n => n % 3 === 0
+const divisibleBy5 = n => n % 5 === 0
 
-export default (number) => divisibleBy3(number) ? "fizz" : String(number);
+export default (number) => {
+  if (divisibleBy3(number))  return "fizz";
+  if (divisibleBy5(number))  return "buzz";
+  return String(number)
+};
 
--- fizzbuzz.spec.js
+++ fizzbuzz.spec.js
@@ -11,7 +11,9 @@ describe("fizzbuzz", () => {
   it("returns a fizz for a number that is divisible by 3 (6 => fizz)", () => {
     expect(fizzbuzz(6)).toEqual("fizz");
   });
-  it.todo("returns a buzz for a number that is divisible by 5 (5 => buzz)");
+  it("returns a buzz for a number that is divisible by 5 (5 => buzz)", () => {
+    expect(fizzbuzz(5)).toEqual("buzz");
+  });
   it.todo("returns a buzz for a number that is divisible by 5 (10 => buzz)");
   it.todo("returns fizzbuzz for a number that is divisible by 3 and 5 (15 => fizzbuzz)");
 });
