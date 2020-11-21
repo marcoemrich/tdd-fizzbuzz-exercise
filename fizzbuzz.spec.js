119f3afd472ee6a82cd1d9f3b05fabacaa123e97
Marco Emrich <marco.emrich@codecentric.de>
2020-11-21 16:22:43 +0100

    it6-parametrized

--- fizzbuzz.spec.js
+++ fizzbuzz.spec.js
@@ -1,22 +1,16 @@
 import fizzbuzz from "./fizzbuzz.js";
 
 describe("fizzbuzz", () => {
-  it("returns a number as string for no special cases (1 => 1)", () => {
-    expect(fizzbuzz(1)).toEqual("1");
-  });
-  it("returns a fizz for a number that is divisible by 3 (3 => fizz)", () => {
-    expect(fizzbuzz(3)).toEqual("fizz");
-  });
-  it("returns a fizz for a number that is divisible by 3 (6 => fizz)", () => {
-    expect(fizzbuzz(6)).toEqual("fizz");
-  });
-  it("returns a buzz for a number that is divisible by 5 (5 => buzz)", () => {
-    expect(fizzbuzz(5)).toEqual("buzz");
-  });
-  it("returns a buzz for a number that is divisible by 5 (10 => buzz)", () => {
-    expect(fizzbuzz(10)).toEqual("buzz");
-  });
-  it("returns fizzbuzz for a number that is divisible by 3 and 5 (15 => fizzbuzz)", () => {
-    expect(fizzbuzz(15)).toEqual("fizzbuzz");
+  test.each([
+    [1, "1"],
+    [11, "11"],
+    [3, "fizz"],
+    [6, "fizz"],
+    [5, "buzz"],
+    [10, "buzz"],
+    [15, "fizzbuzz"],
+    [30, "fizzbuzz"],
+  ])("fizzbuzz(%i) => %s", (n, expected) => {
+    expect(fizzbuzz(n)).toEqual(expected);
   });
 });
