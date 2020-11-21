8c82f66762649114fbb80f3ab372f9a09d2c90ad
Marco Emrich <marco.emrich@codecentric.de>
2020-11-21 15:02:11 +0100

    create exercise

---
+++ fizzbuzz.spec.js
@@ -0,0 +1,8 @@
+describe("fizzbuzz", () => {
+  it.todo("returns a number as string for no special cases (1 => 1)");
+  it.todo("returns a fizz for a number that is divisible by 3 (3 => fizz)");
+  it.todo("returns a fizz for a number that is divisible by 3 (6 => fizz)");
+  it.todo("returns a buzz for a number that is divisible by 5 (5 => buzz)");
+  it.todo("returns a buzz for a number that is divisible by 5 (10 => buzz)");
+  it.todo("returns fizzbuzz for a number that is divisible by 3 and 5 (15 => fizzbuzz)");
+});
