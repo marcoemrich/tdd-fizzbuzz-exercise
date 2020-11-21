4c47e9458e09cf9f6e8b06f50683e8a1993aaf49
Marco Emrich <marco.emrich@codecentric.de>
2020-11-21 17:26:50 +0100

    add additional requirements

--- fizzbuzz.spec.js
+++ fizzbuzz.spec.js
@@ -4,5 +4,11 @@ describe("fizzbuzz", () => {
   it.todo("returns a fizz for a number that is divisible by 3 (6 => fizz)");
   it.todo("returns a buzz for a number that is divisible by 5 (5 => buzz)");
   it.todo("returns a buzz for a number that is divisible by 5 (10 => buzz)");
-  it.todo("returns fizzbuzz for a number that is divisible by 3 and 5 (15 => fizzbuzz)");
+  it.todo(
+    "returns fizzbuzz for a number that is divisible by 3 and 5 (15 => fizzbuzz)"
+  );
 });
+
+// last step: parametrize spec with it.each
+// optional: add /7 => Bizz
+// optional: return an array with results for 1..100
